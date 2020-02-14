4c60a93b0b1c5a1def0c6330f1d593f3d4fd9cdc
Heidi Keskitalo SOFTK20P <heidi.keskitalo@centria.fi>
2020-02-14 12:56:11 +0200

    Exercise 17 done

--- part1/calculations/exercise_17/Program.cs
+++ part1/calculations/exercise_17/Program.cs
@@ -6,6 +6,13 @@ namespace exercise_17
   {
     public static void Main(string[] args)
     {
+      Console.WriteLine("Give the first number!");
+      string value1 = Console.ReadLine();
+      int intValue = Convert.ToInt32(value1);
+      Console.WriteLine("Give the second number!");
+      string value2 = Console.ReadLine();
+      int intValue2 = Convert.ToInt32(value2);
+      Console.Write("The sum is " + (intValue + intValue2));
 
       // Write your code here:
 
