343a75cb107133ebd7e8cfc2a4d69c07a57c91d1
Heidi <info@risako.fi>
2020-02-16 18:24:52 +0200

    Exercise 26 done

--- part1/conditionals/exercise_26/Program.cs
+++ part1/conditionals/exercise_26/Program.cs
@@ -7,7 +7,13 @@ namespace exercise_26
     public static void Main(string[] args)
     {
       // Write your code here:
-
+      Console.WriteLine("Give your age:");
+      string age = Console.ReadLine();
+      int speed = Convert.ToInt32(age);
+      if (speed < 1900)
+     {
+        Console.WriteLine("You're old");
+      } 
     }
   }
 }
