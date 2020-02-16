6dc044f53a8d1525a344b72c76f03352db34b46d
Heidi <info@risako.fi>
2020-02-16 20:26:45 +0200

    Exercise 33 done

--- part1/conditionals/exercise_33/Program.cs
+++ part1/conditionals/exercise_33/Program.cs
@@ -7,7 +7,19 @@ namespace exercise_33
     public static void Main(string[] args)
     {
       // Write your code here:
+      Console.WriteLine("Give the first string:");
+      string string1 = Console.ReadLine();
+      Console.WriteLine("Give the second string:");
+      string string2 = Console.ReadLine();
 
+      if (string1 == string2)
+      { 
+        Console.WriteLine("Echo!");
+      }
+      else 
+      {
+        Console.WriteLine("Nope!");
+      }
     }
   }
 }
