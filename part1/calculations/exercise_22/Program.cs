2873a44afdfee0d52465cde81adf00c40ca543d9
Heidi <info@risako.fi>
2020-02-16 17:31:03 +0200

    Exercise 23 done

--- part1/calculations/exercise_22/Program.cs
+++ part1/calculations/exercise_22/Program.cs
@@ -4,9 +4,17 @@ namespace exercise_22
 {
   class Program
   {
-    public static void Main(string[] args)
     {
 
+      
+      Console.WriteLine("Give the first number!");
+      string value1 = Console.ReadLine();
+      int intValue = Convert.ToInt32(value1);
+      Console.WriteLine("Give the second number!");
+      string value2 = Console.ReadLine();
+      int intValue2 = Convert.ToInt32(value2);
+      double avarage = (intValue + intValue2) / 2.0;
+      Console.WriteLine("The average is " + avarage);
       // Write your code here:
 
     }
