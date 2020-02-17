e0b2d13361267166a544af2683bb82f645e04b58
Heidi <info@risako.fi>
2020-02-17 15:27:16 +0200

    Exercise 44 done

--- part2/subproblems/exercise_44/Program.cs
+++ part2/subproblems/exercise_44/Program.cs
@@ -8,7 +8,23 @@ namespace exercise_44
     {
 
       // Write your code here:
+      int first = Convert.ToInt32(Console.ReadLine());
+      int second = Convert.ToInt32(Console.ReadLine());
+      if (first > second)
+      {
+        Console.WriteLine(first +  " is greater than " +  second + ".");
+      }
+    
+      else if (first < second)
+      {
+        Console.WriteLine(first + " is less than " + second + ".");
+      }
 
+      else if (first == second)
+      {
+        Console.WriteLine(first + " is equal to " + second + ".");
+      }
+      
     }
   }
 }
