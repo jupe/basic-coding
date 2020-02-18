721ab44dba0a8f34064324eeb18280c2aa849347
Heidi <info@risako.fi>
2020-02-18 10:33:51 +0200

    Exercise 48 Not ready

--- part2/moreLoops/exercise_48/Program.cs
+++ part2/moreLoops/exercise_48/Program.cs
@@ -8,7 +8,40 @@ namespace exercise_48
     {
 
       // Write your code here:
- 
+      Console.WriteLine("Give numbers:");
+      int summa = 0; 
+      int numbers = 0;
+      int even = 0;
+      int odd = 0;
+
+      while (true)
+
+     {
+        int intValue1 = Convert.ToInt32(Console.ReadLine());
+        if (intValue1 == -1)
+        {
+          break;
+        }
+        summa = summa + intValue1;
+        numbers++;
+        if (intValue1 % 2 == 0)
+        {
+          even++;
+        }
+        else
+        {
+          odd++;
+        }
+        
+     }
+
+     double avarage = (double)summa / numbers;   
+      Console.WriteLine("Thx! Bye!");
+      Console.WriteLine("Sum: " + summa);
+      Console.WriteLine("Numbers: " + numbers);
+      Console.WriteLine("Avarage: " + avarage);
+      Console.WriteLine("Even: " + even);
+      Console.WriteLine("Odd: " + odd);
     }
   }
 }
