4f58bddb7921cdeeb4b6e9b20daf436d7d54fe0c
Heidi Keskitalo SOFTK20P <heidi.keskitalo@centria.fi>
2020-02-19 10:57:04 +0200

    Exercise 58 done

--- part2/methods/exercise_58/Program.cs
+++ part2/methods/exercise_58/Program.cs
@@ -4,11 +4,24 @@ namespace exercise_58
 {
   class Program
   {
-    public static void Main(String[] args)
-    {
+
+    public static int Smallest(int number1, int number2)
+     {      
+      if (number1 < number2)
+      {
+          return number1;
+      }
+         else
+      {
+          return number2;
+      }
+     }
+     public static void Main(String[] args)
+         {
       int answer = Smallest(2, 7);
       Console.WriteLine("Smallest: " + answer);
     }
+    
 
     // Write your method here:
     
