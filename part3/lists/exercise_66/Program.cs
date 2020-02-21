4abd20d6134f5bfb964d3e27bb35807ed545a655
Heidi Keskitalo SOFTK20P <heidi.keskitalo@centria.fi>
2020-02-21 10:32:59 +0200

    Exercise 66 done

--- part3/lists/exercise_66/Program.cs
+++ part3/lists/exercise_66/Program.cs
@@ -6,18 +6,24 @@ namespace exercise_66
   class Program
   {
     public static void Main(string[] args)
-    {
+    { int i = 0;
       List<string> list = new List<string>();
       while (true)
       {
         string input = Console.ReadLine();
         if (input == "")
         {
+          
+          Console.WriteLine(list[i - 1]);
           break;
         }
+        
         list.Add(input);
+        i++;
       }
-
-    }
+      
+     
+      
+      }
   }
 }
