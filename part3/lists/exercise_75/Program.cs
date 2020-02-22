416de375d01a244e0e95b8fb27808ad1056fb315
Heidi <info@risako.fi>
2020-02-22 17:16:04 +0200

    Exercise 75 done

--- part3/lists/exercise_75/Program.cs
+++ part3/lists/exercise_75/Program.cs
@@ -5,22 +5,40 @@ namespace exercise_75
 {
   class Program
   {
+    
     public static void Main(string[] args)
     {
-
+      List<int> numbers = new List<int>();
+      numbers.Add(3);
+      numbers.Add(2);
+      numbers.Add(6);
+      numbers.Add(-1);
+      numbers.Add(5);
+      numbers.Add(1);
 
       // Example method calls for testing your method.
       // List<int> numbers = new List<int>();
       // numbers.Add(5);
-
+      
       Console.WriteLine("The numbers in the range [0, 5]");
       PrintNumbersInRange(numbers, 0, 5);
-
+      
       Console.WriteLine("The numbers in the range [3, 10]");
       PrintNumbersInRange(numbers, 3, 10);
+    }  
+    public static void PrintNumbersInRange(List<int> numbers, int lowestLimit, int upperLimit)
+    {
+      foreach (int number in numbers)
+      {
+        if (number >= lowestLimit && number <=upperLimit)
+        {
+          Console.WriteLine(number);
+        }
+      }
+              
     }
-
-    }
+    
   }
 }
+
 
