97e754ffc4b1985232a2983024fd17a69fa53bc7
Heidi <info@risako.fi>
2020-02-22 18:04:21 +0200

    Exercise 77 done

--- part3/lists/exercise_77/Program.cs
+++ part3/lists/exercise_77/Program.cs
@@ -7,9 +7,27 @@ namespace exercise_77
   {
     public static void Main(string[] args)
     {
+      List<string> strings = new List<string>();
+      strings.Add("First");
+      strings.Add("Second");
+      strings.Add("Third");
+
+      strings.ForEach(Console.WriteLine);
+      RemoveLast(strings);
+      strings.ForEach(Console.WriteLine);
       // You can try your method here if you want
     }
-
+    public static void RemoveLast(List<string> strings)
+    { 
+      int x = 0;
+      int i = Convert.ToInt32(x);
+      while (i < strings.Count)
+      {
+        i++;
+      }
+      strings.RemoveAt(i - 1);
+      return;
+    }
   }
 }
 
