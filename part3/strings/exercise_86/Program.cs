2b3d102a0609b2740de6d35786648840f6e51c38
Heidi <info@risako.fi>
2020-02-23 17:32:22 +0200

    Exercise 86 done

--- part3/strings/exercise_86/Program.cs
+++ part3/strings/exercise_86/Program.cs
@@ -7,8 +7,19 @@ namespace exercise_86
   {
     public static void Main(string[] args)
     {
-
-
+      while (true)
+      {
+        string words = Console.ReadLine();
+        if (words == "")
+      {
+        break;
+      }  
+        string[] parts = words.Split(" ");
+        foreach (string line in parts)
+        {
+            Console.WriteLine(line);
+        }
+      }
     }
   }
 }
