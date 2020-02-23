6ec6cae7853785611b419c4fcedbb2ebd396d5c1
Heidi <info@risako.fi>
2020-02-23 17:20:13 +0200

    Exercise 85 done

--- part3/strings/exercise_85/Program.cs
+++ part3/strings/exercise_85/Program.cs
@@ -7,8 +7,23 @@ namespace exercise_85
   {
     public static void Main(string[] args)
     {
+      Console.WriteLine("Enter username:");
+      string userName = Console.ReadLine();
+      Console.WriteLine("Enter password:");
+      string passWord = Console.ReadLine();
 
-
+      if (userName == "alex" && passWord == "sunshine") 
+      {
+        Console.WriteLine("You have successfully logged in!");
+      }
+      else if (userName == "emma" && passWord == "haskell")
+      {
+        Console.WriteLine("You have successfully logged in!");
+      }
+      else
+      {
+        Console.WriteLine("Incorrect username or password!");
+      }
     }
   }
 }
