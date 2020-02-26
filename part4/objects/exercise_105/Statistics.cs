b6114c4f33d1a0bcf9d585889a572ca963a667c7
Heidi <info@risako.fi>
2020-02-26 08:19:05 +0200

    Exercise 105 done

--- part4/objects/exercise_105/Statistics.cs
+++ part4/objects/exercise_105/Statistics.cs
@@ -8,11 +8,17 @@ namespace exercise_105
     public Statistics()
     {
       // initialize the variables here
+      this.count = 0;
+      this.sum = 0;
+      
+
     }
 
     public void AddNumber(int number)
     {
       // write code here
+      this.count++;
+      this.sum = sum + number;
       
     }
   }
