fd306775726021639344e059860ad777143cc193
Heidi <info@risako.fi>
2020-02-27 09:47:06 +0200

    Exercise 1114 done

--- part4/files/exercise_114/Program.cs
+++ part4/files/exercise_114/Program.cs
@@ -6,7 +6,18 @@ namespace exercise_114
     {
         public static void Main(string[] args)
         {
+           int counter = 0;
+           while (true) 
+           {
+               string line = Console.ReadLine();
 
+               if (line == "end")
+               {
+                   break;
+               }
+               counter++;
+           }
+            Console.WriteLine(counter);
         }
     }
 }
