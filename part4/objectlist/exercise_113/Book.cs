cce177ca3c1df1e00f562f8227258bd3829dba56
Heidi <info@risako.fi>
2020-02-27 19:15:23 +0200

    Exercise 113 done

--- part4/objectlist/exercise_113/Book.cs
+++ part4/objectlist/exercise_113/Book.cs
@@ -7,19 +7,19 @@ namespace exercise_113
   {
 
     public string title { get; set; }
-    public int bages { get; set; }
+    public int pages { get; set; }
     public string year { get; set; }
 
-    public Book(string title, int bages, string Publication)
+    public Book(string title, int pages, string year)
     {
       this.title = title;
-      this.bages = bages;
+      this.pages = pages;
       this.year = year;
     }
 
     public override string ToString()
     {
-      return this.title + ", " + this.bages + " bages" + this.year;
+      return this.title + ", " + this.pages + " pages, " + this.year;
     }
   }
 }
