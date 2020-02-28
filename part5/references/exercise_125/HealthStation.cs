5df50ec44f2b6e8809c7711b26e40a131161d828
Heidi <info@risako.fi>
2020-02-28 19:03:18 +0200

    Exercise 135 done

--- part5/references/exercise_125/HealthStation.cs
+++ part5/references/exercise_125/HealthStation.cs
@@ -5,17 +5,23 @@ namespace exercise_125
     public int weighings { get; private set; }
 
     // Create a constructor here, if needed
+    public HealthStation()
+    {
+      this.weighings = 0;
+    }
 
     public int Weigh(Person person)
     {
       // return the weight of the person passed as the parameter
       // incrase weighings
-      return 0;
+      this.weighings++;
+      return person.weight;
     }
 
     public void Feed(Person person)
     {
       // Increse the weight of the person with one kilogram
+      person.weight = person.weight + 1 ;
     }
   }
 }
