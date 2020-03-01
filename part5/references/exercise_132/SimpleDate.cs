1d7c6349bc0f7cadfc2cf2c3c5f96ebe56daad94
Heidi <info@risako.fi>
2020-03-01 13:49:46 +0200

    Exercise 132 done

--- part5/references/exercise_132/SimpleDate.cs
+++ part5/references/exercise_132/SimpleDate.cs
@@ -16,18 +16,39 @@ namespace exercise_132
     public void Advance()
     {
       // Do something here
-
+      if (this.day == 30)
+      {
+        this.day = 1;
+        if (this.month == 12)
+        {
+          this.month = 1;
+          this.year++;
+        }
+        else
+        {
+          this.month++;
+        }
+      }
+      else
+      {
+          this.day++;
+      }
     }
 
     public void Advance(int howManyDays)
     {
       // Do something here
+      for (int i = 0; i < howManyDays; i++)
+      {
+        this.Advance();
+      }
     }
 
     public SimpleDate AfterNumberOfDays(int days)
     {
-      SimpleDate newDate = new SimpleDate(/* Do something here?*/);
+      SimpleDate newDate = new SimpleDate(this.day, this.month, this.year);
       // Do something here
+      newDate.Advance(days);
 
       return newDate;
     }
