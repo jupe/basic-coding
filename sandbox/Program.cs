86a6e27870c2d67ba0ce586d0b4c563163498c74
Heidi Keskitalo SOFTK20P <heidi.keskitalo@centria.fi>
2020-03-06 12:55:49 +0200

    Exercise 22 done

--- sandbox/Program.cs
+++ sandbox/Program.cs
@@ -6,7 +6,14 @@ namespace sandbox
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
+            int eka = (1+1);
+            int toka = eka + 3 * (2 + 5);
+            eka = 5;
+            int kolmas = eka + toka;
+            Console.WriteLine(eka);
+            Console.WriteLine(toka);
+            Console.WriteLine(kolmas);
         }
     }
 }
