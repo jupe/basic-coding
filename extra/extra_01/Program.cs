827c086ea710e17688136371c4402932316d77ee
Heidi Keskitalo SOFTK20P <heidi.keskitalo@centria.fi>
2020-03-11 16:22:26 +0200

    Extra 01 done

--- extra/extra_01/Program.cs
+++ extra/extra_01/Program.cs
@@ -7,7 +7,22 @@ namespace extra_01
     public static void Main(string[] args)
     {
       // Add your code here:
+      Console.WriteLine("What do you want to print?");
+      string message = Console.ReadLine();
+      Console.WriteLine("How many times do you want to print?");
+      int number = Convert.ToInt32(Console.ReadLine());
+      int count = 0;
 
-    }
-  }
+      while (true)
+      {
+        Console.WriteLine(message);
+        count++;
+      
+        if (count == number)
+      {
+        break;
+      }    
+      }
+    }  
+  }  
 }
--- sandbox/Program.cs
+++ sandbox/Program.cs
@@ -2,33 +2,65 @@
 
 namespace sandbox
 {
-    class Program
+
+  class Program
+  {
+    public static void Main(String[] args)
     {
-        static void Main(string[] args)
-        {
-           MultiplicationTable(10);
-
-        }        
-        public static void MultiplicationTable(int max)
-        {
-            int numero = 1;
-            while (numero <= max)
-            {
-                PrintMultiplicationTableRow(numero, max);
-                numero++;
-            }
-
-        }
-        public static void PrintMultiplicationTableRow(int numero, int coefficient)
-        {
-            int printable = numero;
-            while (printable <= numero * coefficient)
-            {
-                Console.Write(" " + printable);
-                printable += numero;
-            }
-            Console.WriteLine("");
-        }
+    ChristmasTree (10);
+    Console.WriteLine(" ");
+    //PrintRightTriangle(4);
+    //ChristmasTree (4);
 
     }
+
+    public static void PrintStars(int number)
+    {
+      for (int i = 0; i < number; i++)
+      {
+        Console.Write("*");
+      }
+      Console.WriteLine("");
+    
+    }
+
+    public static void PrintSpaces(int number)
+    {
+      for (int i = 0; i < number; i++)
+      {
+      Console.Write(" ");
+      }
+    }
+
+    public static void PrintRightTriangle(int size)
+    {
+      int left = size -1;
+      for (int i = 1; i <= size; i++)
+             
+      {
+        PrintSpaces(left);
+        PrintStars(i);
+        left--;
+      }
+        
+    }
+
+
+    public static void ChristmasTree(int height)
+    {
+      
+      for (int i =1; i <= height; i++)
+      {
+        PrintSpaces(height - i);
+        PrintStars((2 * i) -1);
+       
+      }
+      PrintSpaces(height - 2);
+      PrintStars(3);
+      PrintSpaces(height - 2);
+      PrintStars(3);
+    }
+  }
 }
+       
+
