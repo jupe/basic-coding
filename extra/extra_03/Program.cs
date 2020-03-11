1ad65bba7d9ef5f4cc598f44359418dd4213e881
Heidi Keskitalo SOFTK20P <heidi.keskitalo@centria.fi>
2020-03-11 16:46:55 +0200

    Extra 03 not ready

--- extra/extra_03/Program.cs
+++ extra/extra_03/Program.cs
@@ -7,7 +7,10 @@ namespace extra_03
     public static void Main(string[] args)
     {
       // Add your code here:
-
+      Console.WriteLine("How many numbers?");
+      int numbers = Convert.ToInt32(Console.ReadLine());
+      Console.WriteLine("Give " + numbers + " numbers:");
+      
     }
   }
 }
