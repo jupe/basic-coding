1353c61bd82f60f5cebdec1af11e156f352d2705
Heidi Keskitalo SOFTK20P <heidi.keskitalo@centria.fi>
2020-03-11 16:39:20 +0200

    Extra 02 done

--- extra/extra_02/Program.cs
+++ extra/extra_02/Program.cs
@@ -7,7 +7,22 @@ namespace extra_02
     public static void Main(string[] args)
     {
       // Add your code here:
-
+      Console.WriteLine("Give three numbers:");
+      int number1 = Convert.ToInt32(Console.ReadLine());
+      int number2 = Convert.ToInt32(Console.ReadLine());
+      int number3 = Convert.ToInt32(Console.ReadLine());
+      if (number1 > number2 & number1 > number3)
+      {
+        Console.WriteLine("Largest: " + number1);
+      }
+      else if (number1 < number2 & number2 > number3)
+      {
+        Console.WriteLine("Largest: " + number2);
+      }
+      else 
+      {
+        Console.WriteLine("Largest: " + number3);
+      }
     }
   }
 }
