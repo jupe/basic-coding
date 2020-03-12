82cb93b7ecb2479c9f392fc2f897b8cba85060ef
Heidi Keskitalo SOFTK20P <heidi.keskitalo@centria.fi>
2020-03-12 12:52:20 +0200

    Extra 08 done

--- extra/extra_08/Program.cs
+++ extra/extra_08/Program.cs
@@ -7,7 +7,55 @@ namespace extra_08
     public static void Main(string[] args)
     {
       // Add your code here:
+      ChristmasTree (10);
+      Console.WriteLine(" ");
+      //ChristmasTree (4);
 
+    }
+
+    public static void PrintStars(int number)
+    {
+      for (int i = 0; i < number; i++)
+      {
+        Console.Write("*");
+      }
+      Console.WriteLine("");
+    
+    }
+
+    public static void PrintSpaces(int number)
+    {
+      for (int i = 0; i < number; i++)
+      {
+      Console.Write(" ");
+      }
+    }
+    public static void PrintRightTriangle(int size)
+    {
+      int left = size -1;
+      for (int i = 1; i <= size; i++)
+             
+      {
+        PrintSpaces(left);
+        PrintStars(i);
+        left--;
+      } 
+    }
+
+
+    public static void ChristmasTree(int height)
+    {
+      
+      for (int i =1; i <= height; i++)
+      {
+        PrintSpaces(height - i);
+        PrintStars((2 * i) -1);
+       
+      }
+      PrintSpaces(height - 2);
+      PrintStars(3);
+      PrintSpaces(height - 2);
+      PrintStars(3);
     }
   }
 }
