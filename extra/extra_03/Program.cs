5f3227edabf3b787fd532777cbb0cfa19420319c
Heidi Keskitalo SOFTK20P <heidi.keskitalo@centria.fi>
2020-03-12 10:36:18 +0200

    Extra 04 done

--- extra/extra_03/Program.cs
+++ extra/extra_03/Program.cs
@@ -8,9 +8,28 @@ namespace extra_03
     {
       // Add your code here:
       Console.WriteLine("How many numbers?");
-      int numbers = Convert.ToInt32(Console.ReadLine());
-      Console.WriteLine("Give " + numbers + " numbers:");
+      int sum = 0;
+      int count = 0;
+      while (true)
+      {
+        
+        int numbers = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Give " + numbers + " numbers:");
+        if (numbers == 0)
+        {
+          int numbers2 = Convert.ToInt32(Console.ReadLine());
+        }
+        
+        else 
+        {
+          sum = sum + numbers;
+          count++;
+        }  
+      }
       
+      Console.WriteLine("Their sum: " + sum);
+      Console.WriteLine("Their total: " + count);
+      Console.WriteLine("Their average: " + (double) sum / count);
     }
   }
 }
