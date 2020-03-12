882f0492fd70a6a3a8f07408898490d900ec37a0
Heidi Keskitalo SOFTK20P <heidi.keskitalo@centria.fi>
2020-03-12 13:49:58 +0200

    Extra 10 done

--- extra/extra_10/Program.cs
+++ extra/extra_10/Program.cs
@@ -7,7 +7,21 @@ namespace extra_10
     public static void Main(string[] args)
     {
       // Add your code here:
+      int[] array = { 5, 1, 3, 4, 2 };
+      PrintArrayInStars(array);
 
+    }
+
+    public static void PrintArrayInStars(int[] array)
+    {
+      for (int i = 0; i < array.Length; i++)
+      {
+        for (int star = 0; star < array[i]; star++) 
+        {
+          Console.Write("*");
+        }
+      Console.WriteLine();
+      }
     }
   }
 }
