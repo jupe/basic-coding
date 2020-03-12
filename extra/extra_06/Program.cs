e8b01f7ba739865734ed16cd917b6b19b357fa42
Heidi Keskitalo SOFTK20P <heidi.keskitalo@centria.fi>
2020-03-12 12:02:43 +0200

    Exercise 06 donee

--- extra/extra_06/Program.cs
+++ extra/extra_06/Program.cs
@@ -6,8 +6,20 @@ namespace extra_06
   {
     public static void Main(string[] args)
     {
+      
       // Add your code here:
-
+      int num = Convert.ToInt32(Console.ReadLine());
+      int num2 = Convert.ToInt32(Console.ReadLine());
+      double squareRoot = Math.Sqrt(num + num2);
+      
+      if (squareRoot > 0)
+      {
+        Console.WriteLine(squareRoot);
+      }
+      else 
+      {
+        Console.WriteLine("0");
+      }
     }
   }
 }
