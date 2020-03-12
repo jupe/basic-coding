b2f4a193db51dbd6c45c4d588d0ddb3caebc0eaf
Heidi Keskitalo SOFTK20P <heidi.keskitalo@centria.fi>
2020-03-12 11:38:30 +0200

    Extra 05 done

--- extra/extra_05/Program.cs
+++ extra/extra_05/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace extra_05
 {
@@ -7,7 +8,30 @@ namespace extra_05
     public static void Main(string[] args)
     {
       // Add your code here:
-
+      for (int i = 1; i < 6; i++)
+      {
+        Console.WriteLine(i);
+      }
+      Console.WriteLine("Give words, 'end' quits:");
+      List<string> list = new List<string>();
+      string input = "";
+           
+      while (true)
+      {
+        input = Console.ReadLine();
+        if (input == "end")
+        {
+          break;
+        }
+        else
+        {
+          list.Add(input);
+        }
+        
+      } 
+      list.ForEach(Console.WriteLine);
     }
+
+    
   }
 }
