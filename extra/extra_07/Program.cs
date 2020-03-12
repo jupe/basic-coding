3f4789a8aa16aead397fcb7e8341186cd8847fbc
Heidi Keskitalo SOFTK20P <heidi.keskitalo@centria.fi>
2020-03-12 12:39:58 +0200

    Exercise 07 not ready

--- extra/extra_07/Program.cs
+++ extra/extra_07/Program.cs
@@ -7,7 +7,21 @@ namespace extra_07
     public static void Main(string[] args)
     {
       // Add your code here:
+      Console.WriteLine("Give integers, 'end' quits:");
+      int sum = 0;
+      
+      while (true)
 
+      {
+        int numbers = Convert.ToInt32(Console.ReadLine());
+        string input = Console.ReadLine();
+        if (input == "end")
+        {
+          break;
+        }
+        sum = sum + numbers;
+      }
+      Console.WriteLine("Sum: " + sum);
     }
   }
 }
