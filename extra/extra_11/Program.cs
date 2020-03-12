8f0e775bccec6b2e9bac924bebdb76b8e2dacdbd
Heidi Keskitalo SOFTK20P <heidi.keskitalo@centria.fi>
2020-03-12 13:54:05 +0200

    Extra 11 done

--- extra/extra_11/Program.cs
+++ extra/extra_11/Program.cs
@@ -7,7 +7,20 @@ namespace extra_11
     public static void Main(string[] args)
     {
       // Add your code here:
-
+      Console.WriteLine("Give a sentence:");
+      while (true)
+      {
+        string words = Console.ReadLine();
+        if (words == "")
+      {
+        break;
+      }  
+        string[] parts = words.Split(" ");
+        foreach (string line in parts)
+        {
+            Console.WriteLine(line);
+        }
+      }
     }
   }
 }
