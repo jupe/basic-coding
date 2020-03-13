8442dfa5bee4358f289d2535cc8230e76b76bfb9
Heidi <info@risako.fi>
2020-03-13 12:27:04 +0200

    Extra 09 done

--- extra/extra_09/Program.cs
+++ extra/extra_09/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace extra_09
 {
@@ -7,7 +8,31 @@ namespace extra_09
     public static void Main(string[] args)
     {
       // Add your code here:
+      List<string> list = new List<string>();
+      list.Add("This is a string");
+      list.Add("This is magic");
+      list.Add("THIS IS PATRICK");
 
+      // for loop
+      //Console.WriteLine("forloop");
+      for (int i = 0; i < list.Count; i++)
+      {
+        Console.WriteLine(list[i]);
+      }
+
+      // While loop
+      //Console.WriteLine("\nwhile loop");
+      int index = 0;
+      while (index < list.Count)
+      {
+        Console.WriteLine(list[index]);
+        index++;
+      }
+
+      //foreach loop
+      //Console.WriteLine("\nforeach loop");
+      list.ForEach(Console.WriteLine);
+      
     }
   }
 }
