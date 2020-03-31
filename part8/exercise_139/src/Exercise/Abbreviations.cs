bec9bc8ed7263f202cf464fef272b9976579b7e0
Heidi <info@risako.fi>
2020-03-31 14:45:14 +0300

    Exercise 139 done

--- part8/exercise_139/src/Exercise/Abbreviations.cs
+++ part8/exercise_139/src/Exercise/Abbreviations.cs
@@ -4,20 +4,45 @@ namespace Exercise
 {
   public class Abbreviations
   {
+    // add dictionary for abbrevs
+    private Dictionary<string, string> abbrevs;
 
+    //add empty constructor
+    public Abbreviations()
+    {
+      this.abbrevs = new Dictionary<string,string>();
+    }
+
+    //add to abbrevs
     public void AddAbbreviation(string abbreviation, string explanation)
     {
-
+      this.abbrevs.Add(abbreviation, explanation);
     }
 
+    //check if abbrevs contain give string
     public bool HasAbbreviation(string abbreviation)
     {
-      return false;
+      if (this.abbrevs.ContainsKey(abbreviation))
+      { 
+        return true;
+      }
+      else
+      {
+        return false;
+      }
     }
 
+    //return abbrev, if none return not found
     public string FindExplanationFor(string abbreviation)
     {
-      return null;
+      if (HasAbbreviation(abbreviation))
+    {
+      return abbrevs[abbreviation];
+    }
+    else
+    {
+      return "not found";
+    }
     }
   }
 }
