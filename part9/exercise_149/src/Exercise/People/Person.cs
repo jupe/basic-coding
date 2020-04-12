2b0da9f1dc7d1b97b60e800cd4e63fb3f8c0eb6f
Heidi <info@risako.fi>
2020-04-12 12:22:05 +0300

    Exercise 149 done

--- part9/exercise_149/src/Exercise/People/Person.cs
+++ part9/exercise_149/src/Exercise/People/Person.cs
@@ -1,6 +1,22 @@
 namespace Exercise
 {
-  public class Person
+  using System;
+  public class Person 
   {
+    public string name {get; set;}
+    public string address {get; set;}
+    
+
+    public Person(string name, string address)
+    {
+      this.name = name;
+      this.address = address;
+
+    }
+   
+    public override string ToString()
+    {
+      return this.name + ", " +  this.address;
+    }
   }
 }
