644e84247bb62d9292cc2452107bd07dcbafa9ad
Heidi <info@risako.fi>
2020-04-15 10:53:53 +0300

    Exersice 152 done

--- part9/exercise_152/src/Exercise/Tacos/ITacoBox.cs
+++ part9/exercise_152/src/Exercise/Tacos/ITacoBox.cs
@@ -1,13 +1,9 @@
 namespace Exercise
 {
-  using System;
-  using System.Collections.Generic;
   public interface ITacoBox
   {
-    
     int TacosRemaining();
     
     public void Eat();
-    
   }
 }
