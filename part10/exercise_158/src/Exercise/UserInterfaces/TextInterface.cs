0435bdfdf250e5f6d2f73826373bdc6388cecf74
Heidi <info@risako.fi>
2020-04-20 13:56:43 +0300

    Exercise 158 done

--- part10/exercise_158/src/Exercise/UserInterfaces/TextInterface.cs
+++ part10/exercise_158/src/Exercise/UserInterfaces/TextInterface.cs
@@ -8,10 +8,33 @@ namespace Exercise
     private List<Book> books;
     public TextInterface()
     {
+      this.books = new List<Book>();
+
     }
 
     public void Start()
     {
+      while (true)
+      {
+        Console.WriteLine("Input the name of the book, empty stops:");
+        string name = Console.ReadLine();
+        if (name == "")
+        {
+          break;
+        }
+        Console.WriteLine("Input the age recommendation:");
+        int age = Convert.ToInt32(Console.ReadLine());
+        this.books.Add(new Book(name, age));
+      }
+     
+      
+      Console.WriteLine(this.books.Count + " books in total.");
+      Console.WriteLine();
+      this.books.Sort();
+      Console.WriteLine("Books:");
+      this.books.ForEach(Console.WriteLine);
+       
     }
-  }
+  }   
 }
+
