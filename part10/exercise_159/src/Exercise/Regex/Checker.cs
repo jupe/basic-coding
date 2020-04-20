d66f9908d9313a1b047e150b61fcebdb220712b9
Heidi <info@risako.fi>
2020-04-20 15:17:17 +0300

    Exercise 159 done

--- part10/exercise_159/src/Exercise/Regex/Checker.cs
+++ part10/exercise_159/src/Exercise/Regex/Checker.cs
@@ -5,16 +5,31 @@ namespace Exercise
   {
     public bool DayOfWeek(string str)
     {
+      Regex regex = new Regex("^(mon|tue|wed|thu|fri|sat|sun)$");
+      if (regex.IsMatch(str))
+      {
+        return true;
+      }  
       return false;
     }
 
     public bool AllVowels(string str)
     {
+      Regex regex = new Regex("^(a|e|i|o|u)*$");
+      if (regex.IsMatch(str))
+      {
+        return true;
+      }  
       return false;
     }
 
     public bool TimeOfDay(string str)
     {
+      Regex regex = new Regex("(([0-1][0-9])|([2][0-3])):([0-5][0-9]):([0-5][0-9])");
+      if (regex.IsMatch(str))
+      {
+        return true;
+      }
       return false;
     }
   }
