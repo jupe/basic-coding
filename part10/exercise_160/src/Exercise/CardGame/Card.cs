1264b3b98c3afdfd201738e57d631195e748b470
Heidi <info@risako.fi>
2020-04-21 08:54:17 +0300

    Exercise 160 done

--- part10/exercise_160/src/Exercise/CardGame/Card.cs
+++ part10/exercise_160/src/Exercise/CardGame/Card.cs
@@ -14,12 +14,46 @@ namespace Exercise
 
     public override string ToString()
     {
-      return "";
+      if (this.value == 11)
+      {
+        return this.suit + " J";
+      }
+      if  (this.value == 12)
+      {
+        return this.suit + " Q";
+      }
+      if (this.value == 13)
+      {
+        return this.suit + " K";
+      }
+      if (this.value == 14)
+      {
+        return this.suit + " A";
+      } 
+      else
+      {
+        return this.suit + " " + this.value;
+      }  
     }
 
     public int CompareTo(Card another)
     {
-      return 0;
+      if (another == null)
+      {
+        return 1;
+      }
+      if (this.value == another.value)
+      {
+        return this.suit.CompareTo(another.suit);
+      }
+      else if (this.value > another.value)
+      {
+        return 1;
+      }
+      else
+      {
+        return -1;
+      }
     }
-  }
+  }  
 }
--- part10/exercise_160/src/Exercise/CardGame/Hand.cs
+++ part10/exercise_160/src/Exercise/CardGame/Hand.cs
@@ -5,23 +5,59 @@ namespace Exercise
   public class Hand : IComparable<Hand>
   {
     private List<Card> hand;
-
+    public Hand()
+    {
+      this.hand = new List<Card>();
+    }
     public void Add(Card card)
     {
+      /// Start here
+      // add cards if they are not already in the list
+      if (!this.hand.Contains(card))
+      {
+        this.hand.Add(card);
+      }
+      
     }
 
     public void Print()
     {
+      foreach (Card card in this.hand)
+      {
+        Console.WriteLine(card.ToString());
+      }
     }
 
     public void Sort()
     {
+      this.hand.Sort();
     }
 
 
     public int CompareTo(Hand hand)
     {
-      return 0;
+      int sumHand1 = 0;
+      foreach (Card card in this.hand)
+      {
+        sumHand1 += card.value;
+      }
+      int sumHand2 = 0;
+      foreach (Card card in hand.hand)
+      {
+        sumHand2 += card.value;
+      }
+      if (sumHand1 == sumHand2)
+      {
+        return 0;
+      }
+      else if (sumHand1 > sumHand2)
+      {
+        return 1;
+      }
+      else
+      {
+        return -1;
+      }
     }
   }
 }
