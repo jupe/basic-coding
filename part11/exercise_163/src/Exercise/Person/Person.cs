3aa0d189faa429611938c7665ca17df957088879
Heidi <info@risako.fi>
2020-04-24 11:38:31 +0300

    Exercise 163 done

--- part11/exercise_163/src/Exercise/Person/Person.cs
+++ part11/exercise_163/src/Exercise/Person/Person.cs
@@ -9,8 +9,17 @@ namespace Exercise
 
     public Person(string name, int age)
     {
+      if (name == "" || name == null || name.Length > 40)
+      {
+      throw new ArgumentException("Name should not be null, or empty or longer than 40");
+      }
+       if (age < 0 || age > 120)
+      {
+      throw new ArgumentException("The age must be between 0 and 120");
+      }
       this.name = name;
       this.age = age;
+      
     }
   }
 }
