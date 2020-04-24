3db55e78c19c69ee69258dc93f6ebe1b4f8e6e52
Heidi <info@risako.fi>
2020-04-24 10:38:37 +0300

    Exercise 162 done

--- part11/exercise_162/src/Exercise/Lottery/LotteryRow.cs
+++ part11/exercise_162/src/Exercise/Lottery/LotteryRow.cs
@@ -20,15 +20,26 @@ namespace Exercise
     public bool ContainsNumber(int number)
     {
       // Tests whether the number is already among the randomized numbers
-      return false;
+      return this.numbers.Contains(number);
     }
 
     public void RandomizeNumbers()
     {
       // initialize the list for numbers
       this.numbers = new List<int>();
-      // Implement the randomization of the numbers by using the method ContainsNumber() here
-    }
-
+      // Implement the randomization of the numbers 
+      //by using the method ContainsNumber() here
+      Random rnd = new Random();
+      while (this.numbers.Count < 7)
+      {
+        int lottery = rnd.Next(1, 41);
+        if (!ContainsNumber(lottery))
+        {
+          this.numbers.Add(lottery);
+        }
+      }
+      //Sort!
+      this.numbers.Sort();
+    } 
   }
 }
