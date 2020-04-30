187232f9e7f588e195e02d97618d4fd99be4fd08
Heidi <info@risako.fi>
2020-04-30 14:10:32 +0300

    Exercise 168 done

--- part12/exercise_168/src/Exercise/Program.cs
+++ part12/exercise_168/src/Exercise/Program.cs
@@ -1,7 +1,8 @@
 namespace Exercise
 {
   // Add "using Master  = ..."
-
+  using System;
+  using Master = MasterProject.MasterClass;
   public class Program
   {
     public static void Main(string[] args)
