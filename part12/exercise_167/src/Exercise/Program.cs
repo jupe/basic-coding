4b79bdf8fdeeae62377e2183ca093e71c4e24b3b
Heidi <info@risako.fi>
2020-04-30 13:40:03 +0300

    Exercise 167 done

--- part12/exercise_167/src/Exercise/Program.cs
+++ part12/exercise_167/src/Exercise/Program.cs
@@ -1,7 +1,10 @@
 namespace Exercise
 {
   // using something...
-
+  using System;
+  using System.Linq;
+  using System.Collections.Generic;
+  using System.Text.RegularExpressions;
   public class Program
   {
     public static void Main(string[] args)
